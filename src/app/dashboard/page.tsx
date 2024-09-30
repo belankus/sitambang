6c5dc523798148b6eda5204ac51e7794fd0e7504
Bellawan Kusuma <belankus@gmail.com>
2024-09-30 16:30:20 +0700

    feat: add line chart

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Line } from "react-chartjs-2";
 import {
   Chart,
   ArcElement,
@@ -8,10 +8,14 @@ import {
   BarController,
   CategoryScale,
   LinearScale,
+  LineController,
+  LineElement,
   BarElement,
   Legend,
   Tooltip,
+  PointElement,
 } from "chart.js";
+
 Chart.register(
   ArcElement,
   Colors,
@@ -19,10 +23,14 @@ Chart.register(
   BarElement,
   CategoryScale,
   LinearScale,
+  LineController,
+  LineElement,
   Legend,
+  PointElement,
   Tooltip,
 );
-const data = {
+
+const dataDonuts = {
   labels: ["Red", "Blue", "Yellow"],
   datasets: [
     {
@@ -38,10 +46,23 @@ const data = {
   ],
 };
 
+const dataLine = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    },
+  ],
+};
+
 export default function Page() {
   return (
     <>
-      <div className="bg-background-2 flex h-[50px] w-full">
+      <div className="flex h-[50px] w-full bg-background-2">
         <div className="mx-auto flex w-full max-w-6xl items-center justify-between">
           <ul className="flex items-center gap-2 text-white">
             <li className="relative block rounded-md">
@@ -50,16 +71,36 @@ export default function Page() {
           </ul>
         </div>
       </div>
-      <section className="bg-background-2 w-full pb-5">
-        <div className="shadow-background-2 mx-10 min-h-screen max-w-full rounded-xl bg-white p-10 [box-shadow:_rgba(0,_0,_0,_0.5)_0px_5px_10px_0px_inset]">
-          Dashboard
-          <div className="size-48">
-            <Doughnut
-              data={data}
-              options={{
-                responsive: true,
-              }}
-            />
+      <section className="w-full bg-background-2 pb-5">
+        <div className="mx-10 min-h-screen max-w-full rounded-xl bg-white p-10 shadow-background-2 [box-shadow:_rgba(0,_0,_0,_0.5)_0px_5px_10px_0px_inset]">
+          <h1 className="font-bold">Dashboard</h1>
+
+          {/* Row 1 */}
+          <div className="w-fullm flex gap-5">
+            {/* Widget */}
+            <div className="w-3/4 rounded-md border bg-white p-5 shadow-md">
+              <h2>Title Line Chart 1</h2>
+              <div className="">
+                <Line
+                  data={dataLine}
+                  options={{
+                    responsive: true,
+                  }}
+                />
+              </div>
+            </div>
+            {/* Widget */}
+            <div className="rounded-md border bg-white p-5 shadow-md">
+              <h2 className="text-xl font-semibold">Title Donuts 1</h2>
+              <div className="size-48">
+                <Doughnut
+                  data={dataDonuts}
+                  options={{
+                    responsive: true,
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </section>
