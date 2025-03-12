f1ea529a2dfefb7a3cead93d0cc733fbb2838f1d
Bellawan Kusuma <belankus@gmail.com>
2025-03-12 08:00:47 +0700

    feat: modify model

--- src/components/fragments/Dashboard/Stockpile/StockpileModel.tsx
+++ src/components/fragments/Dashboard/Stockpile/StockpileModel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import { OrbitControls, useGLTF, useTexture, Grid } from "@react-three/drei";
 import { Suspense, useState } from "react";
 import * as THREE from "three";
 
@@ -33,16 +33,28 @@ const StockpileScene = () => {
 
   return (
     <div className="relative h-screen">
-      <Canvas camera={{ position: [0, -20, 3], fov: 10 }}>
+      <Canvas camera={{ position: [0, -20, 3], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 10]} intensity={1} castShadow />
         <spotLight position={[0, 5, 10]} angle={0.3} intensity={0.8} />
+
         <Suspense fallback={null}>
+          {/* ✅ Grid untuk bidang XY */}
+          <Grid
+            position={[0, 0, 0]} // Pastikan grid di dasar model
+            rotation={[Math.PI / 2, 0, 0]} // Rotasi agar sejajar bidang XY
+            args={[50, 50]} // Ukuran grid (opsional)
+            sectionColor="gray"
+            cellColor="white"
+            cellThickness={0.5}
+          />
+
           <mesh onClick={handleClick} receiveShadow castShadow>
             <StockpileModel />
             <meshStandardMaterial color={"#3B3B3B"} />
           </mesh>
         </Suspense>
+
         <OrbitControls />
       </Canvas>
 
